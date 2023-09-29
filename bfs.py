9f10be19ed48b27382df6df3c3186fee2fef49ed
sanchalee19 <115171947+sanchalee19@users.noreply.github.com>
2023-09-29 20:44:54 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,46 +1,28 @@
-def bfs(matrix , n , start):
-    visited = [False] * n
-    q = []
-    q.append(start)
-    visited[start] = True
-    while q:
-        vis = q.pop(0)
-        print(vis)
-        for node in range(0 , n):
-            if matrix[vis][node] == 1 and not visited[node]:
-                q.append(node)
-                visited[node] = True
+graph = {
+    '1': ['2', '3'],
+    '2': ['4', '5'],
+    '3': ['6'],
+    '4': [],
+    '5': ['6'],
+    '6': []
+}
 
-if __name__ == "__main__":
-    n = int(input("Enter number of nodes : "))
-    matrix = []
-    print("Enter adjacency matrix\n")
-    for i in range(n):
-        a = []
-        for j in range(n):
-            a.append(int(input()))
-        matrix.append(a)
+visited = set()
+queue = []
 
-    start = int(input("Enter starting vertex : "))
-    bfs(matrix , n , start)
+def bfs(visited, graph, node):
+    visited.add(node)
+    queue.append(node)
 
+    while queue: 
+        m = queue.pop(0)
+        print(m, end=" ")
 
-"""
-Test Case ==>
-0 
-1 
-0 
-1
-1 
-0 
-1 
-0
-0 
-1 
-0 
-1
-1 
-0 
-1 
-0
-"""
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+print("BFS is:")
+
+bfs(visited, graph, '1')
