041b00d9f17973dc037e4d2a935b6c1e21c87227
sanchalee19 <115171947+sanchalee19@users.noreply.github.com>
2023-09-29 15:28:37 +0530

    Update kruskals.py

--- kruskals.py
+++ kruskals.py
@@ -1,1 +1,48 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
 
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+def kruskal_mst(graph):
+    graph.graph = sorted(graph.graph, key=lambda item: item[2])
+    result = []
+    parent = []
+    
+    def find_parent(i):
+        if parent[i] == -1:
+            return i
+        if parent[i] != -1:
+            return find_parent(parent[i])
+
+    def union(u, v):
+        root_u = find_parent(u)
+        root_v = find_parent(v)
+        parent[root_u] = root_v
+
+    parent = [-1] * graph.V
+
+    for i in range(graph.V - 1):
+        u, v, w = graph.graph[i]
+        root_u = find_parent(u)
+        root_v = find_parent(v)
+
+        if root_u != root_v:
+            result.append([u, v, w])
+            union(root_u, root_v)
+
+    return result
+
+g = Graph(4)
+g.add_edge(0, 1, 10)
+g.add_edge(0, 2, 6)
+g.add_edge(0, 3, 5)
+g.add_edge(1, 3, 15)
+g.add_edge(2, 3, 4)
+
+mst = kruskal_mst(g)
+print("Edges in MST:")
+for u, v, weight in mst:
+    print(f"{u} - {v} : {weight}")
